974404006ff91aa358ce10ca96a91e746d62da85
Agus9217 <justinibarrola92@gmail.com>
2024-02-26 14:48:11 -0300

    change navbar

--- src/components/CustomLink/index.tsx
+++ src/components/CustomLink/index.tsx
@@ -1,6 +1,5 @@
-
-import { Link as ReactRouterLink, useLocation } from 'react-router-dom';
-import { Link as ChakraLink, LinkProps } from '@chakra-ui/react';
+import { Link as ReactRouterLink, useLocation } from "react-router-dom";
+import { Button as ChakraLink, LinkProps } from "@chakra-ui/react";
 
 interface CustomNavLinkProps extends LinkProps {
   to: string;
@@ -9,17 +8,18 @@ interface CustomNavLinkProps extends LinkProps {
 
 export const CustomLink: React.FC<CustomNavLinkProps> = ({ to, children }) => {
   const location = useLocation();
-  const active = location.pathname
+  const active = location.pathname;
 
   return (
     <ReactRouterLink to={to}>
       <ChakraLink
-        color={active === to ? 'cyan' : undefined}
-        fontWeight={active === to ? 'bold' : undefined}
+        as={"a"}
+        variant={"ghost"}
+        color={active === to ? "yellow.500" : undefined}
+        fontWeight={active === to ? "bold" : undefined}
       >
         {children}
       </ChakraLink>
     </ReactRouterLink>
   );
 };
-
